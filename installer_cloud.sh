a970ef5424cef7fb8f91a157ad130974244c3efe
Andrey <akinava@gmail.com>
2019-11-01 13:27:56 +0300

    Update installer_cloud.sh

--- installer_cloud.sh
+++ installer_cloud.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 sudo apt update
 sudo apt upgrade
-sudo apt install git
+sudo apt install python3 git
 mkdir -p ~/Projects
 cd ~/Projects
 git clone https://github.com/Akinava/PyCrypto.git
+sudo apt install `cat ./PyCrypto/requirements.txt`
+git clone https://github.com/Akinava/UDPHost.git
+cd ./UDPHost/src/
+ln -s ../../PyCrypto/src/pycrypto.py
